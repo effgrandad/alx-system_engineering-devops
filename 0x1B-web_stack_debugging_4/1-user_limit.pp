327883d382fe5e4a7ce94ccbd4068739800b6d95
effgrandad <effgrandad@gmail.com>
2024-09-27 02:17:20 -0700

    commit comments

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
 # Modify the OS configuration to enable the holberton user to log in
 # and open files without receiving an error notice.
-
+# boost limit and soft limit
 
 exec {'OS security config':
   command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
